90368ea654efb364efecbe020466ccae567aa08f
ValentinGuillot <97942568+mrvalbass@users.noreply.github.com>
2024-05-03 12:01:00 +0200

    Add trends mechanic

--- src/components/Trends.js
+++ src/components/Trends.js
@@ -1,17 +1,33 @@
+import { useEffect, useState } from "react";
+
 export default function Trends() {
+  const [hashtags, setHashTags] = useState({});
+
+  useEffect(() => {
+    const getHashtags = async () => {
+      const hashtagsData = await fetch("http://localhost:3000/tweets/hashtags")
+        .then((r) => r.json())
+        .then((data) => data.hashtags);
+      setHashTags(hashtagsData);
+    };
+    getHashtags();
+  }, []);
+
   return (
     <>
       <div className="bg-[#151d26] text-white w-4/12 font-semibold ">
         <h4 className="p-5 text-xl">Trends</h4>
         <div className="bg-[#1b232c] m-4 px-4 py-1">
-          <div className="my-4 py-2">
-            <div className="text-base">#jesuistropchaud</div>
-            <div className="text-xs text-[#65717e]">02 tweets</div>
-          </div>
-          <div>
-            <div className="text-base">#jesuistropchaude</div>
-            <div className="text-xs text-[#65717e]">18009 tweets</div>
-          </div>
+          {Object.keys(hashtags).map((hashtag) => {
+            return (
+              <div className="my-4 py-2">
+                <div className="text-base">#{hashtag}</div>
+                <div className="text-xs text-[#65717e]">
+                  {hashtags[hashtag]} tweets
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </>
