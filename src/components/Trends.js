f75dc77f16597f9c5bdcdfa23b5c91042b90d5b9
ValentinGuillot <97942568+mrvalbass@users.noreply.github.com>
2024-05-05 14:56:48 +0200

    ordered trends list

--- src/components/Trends.js
+++ src/components/Trends.js
@@ -2,13 +2,19 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 
 export default function Trends({ trendsRefresh }) {
-  const [hashtags, setHashTags] = useState({});
+  const [hashtags, setHashtags] = useState([]);
   const router = useRouter();
 
   useEffect(() => {
-    fetch("https://hackatweet-back-theta.vercel.app/tweets/hashtags")
-      .then((r) => r.json())
-      .then((data) => setHashTags(data.hashtags));
+    const getHashtags = async () => {
+      const data = await fetch(
+        "https://hackatweet-back-theta.vercel.app/tweets/hashtags"
+      )
+        .then((r) => r.json())
+        .then((data) => data.hashtags);
+      setHashtags(data);
+    };
+    getHashtags();
   }, [trendsRefresh]);
 
   return (
@@ -16,18 +22,18 @@ export default function Trends({ trendsRefresh }) {
       <div className="bg-[#151d26] text-white w-4/12 font-semibold ">
         <h4 className="p-5 text-xl">Trends</h4>
         <div className="bg-[#1b232c] m-4 rounded-lg">
-          {Object.keys(hashtags).map((hashtag, i, arr) => {
+          {hashtags.map((hashtag, i, arr) => {
             return (
               <div
                 key={i}
-                onClick={() => router.push(`/tag/${hashtag}`)}
+                onClick={() => router.push(`/tag/${hashtag.tag}`)}
                 className={`py-4 px-4 hover:bg-[#232d3b] cursor-pointer ${
                   i === 0 && "rounded-t-lg"
                 } ${i === arr.length - 1 && "rounded-b-lg"}`}
               >
-                <div className="text-base">#{hashtag}</div>
+                <div className="text-base">#{hashtag.tag}</div>
                 <div className="text-xs text-[#65717e]">
-                  {hashtags[hashtag]} tweets
+                  {hashtag.count} tweets
                 </div>
               </div>
             );
